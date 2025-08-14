01609448cebd6f93a01a00993183f21f5abef865
coucoumonecho <coucoumonecho@gmail.com>
2025-08-14 15:16:29 +0800

    dev admin mysql

--- services/admin/infrastructure/database.go
+++ services/admin/infrastructure/database.go
@@ -12,6 +12,9 @@ func InitDB(cfg *AppConfig) (*orm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=true&loc=Local",
 		cfg.Database.User, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.Name,
 	)
+	if cfg.Database.Driver == "" {
+		cfg.Database.Driver = "mysql"
+	}
 	return orm.Open(cfg.Database.Driver, dsn, orm.DBWithMiddlewares(
 		ormotel.NewMiddlewareBuilder(nil).Build(),
 		ormprom.NewMiddlewareBuilder("blog", "admin", "orm", "admin orm latency").Build(),
